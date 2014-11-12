8608564b9d2cfee926476bbd89ea16738f19e302
Max Tritschler <max.tritschler@smartlawmedia.de>
2014-11-12 14:57:10 +0100

    Implemented working book-service and -client.

--- checkout-service/src/main/java/law/smart/library/checkout/CheckoutService.java
+++ checkout-service/src/main/java/law/smart/library/checkout/CheckoutService.java
@@ -3,7 +3,21 @@ package law.smart.library.checkout;
 import law.smart.library.checkout.dto.ReceiptDTO;
 
 public interface CheckoutService {
-    
+
+    /**
+     * If the book with the given ID is available, the book will be marked as loaned.
+     * 
+     * @param id The book ID.
+     * @return A receipt comprising the current loan state and a return date for the book. 
+     */
     ReceiptDTO checkout(String id);
+
+    /**
+     * Retrieves the current loan status and the return date of the book with the given ID.
+     * 
+     * @param id The book ID.
+     * @return A receipt comprising the current loan state and a return date for the book.
+     */
+    ReceiptDTO getStatus(String id);
     
 }
